8dfe357c6d871cb809317733268434efc05bd484
David Crow <jdavidcrow@gmail.com>
2016-09-15 10:52:24 -0700

    [MB-1961] untrack Pod folder

--- Example/Pods/Target Support Files/Pods-UrbanAirship-iOS-GA-Tracker_Tests/Pods-UrbanAirship-iOS-GA-Tracker_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UrbanAirship_iOS_GA_Tracker_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_UrbanAirship_iOS_GA_Tracker_Tests
-@end
